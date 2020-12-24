a456f289b88348c07245c171e92b4e81850b83b0
quentin <quentin.jolyapdev@gmail.com>
2020-12-24 13:03:05 +0100

    add getTime

--- app/src/main/java/com/example/gtf/MainActivity.java
+++ app/src/main/java/com/example/gtf/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.gtf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+    }
+
+    public void openActivity2() {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/gtf/MainActivity2.java
+++ app/src/main/java/com/example/gtf/MainActivity2.java
@@ -2,13 +2,51 @@ package com.example.gtf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    private Button btn;
+
+    private TextView day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity1();
+            }
+        });
+
+        day =findViewById(R.id.tvd);
+
+        Date currentTime = Calendar.getInstance().getTime();
+        String formattedDate = DateFormat.getDateInstance(DateFormat.FULL).format(currentTime);
+
+        String[] splitDate = formattedDate.split(",");
+
+
+        Log.d("myLog", currentTime.toString());
+        Log.d("myLog", formattedDate);
+        day.setText(splitDate[0]);
+
+    }
+
+    public void openActivity1() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
