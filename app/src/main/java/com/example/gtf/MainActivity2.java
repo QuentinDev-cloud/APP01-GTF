8cf0eb3ce9081a9cac51bba49d5b9a448e2ca0f4
quentin <quentin.jolyapdev@gmail.com>
2020-12-29 15:00:02 +0100

    add stopwatch

--- app/src/main/java/com/example/gtf/MainActivity2.java
+++ app/src/main/java/com/example/gtf/MainActivity2.java
@@ -15,7 +15,7 @@ import java.util.Date;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    private Button btn;
+    private Button btnPlay,btnBack;
 
     private TextView day;
 
@@ -23,22 +23,26 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        btn = (Button) findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
+        btnBack = (Button) findViewById(R.id.back);
+        btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openActivity1();
             }
         });
 
-        day =findViewById(R.id.tvd);
+        btnPlay = (Button) findViewById(R.id.play);
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity3();
+            }
+        });
 
+        day =findViewById(R.id.tvd);
         Date currentTime = Calendar.getInstance().getTime();
         String formattedDate = DateFormat.getDateInstance(DateFormat.FULL).format(currentTime);
-
         String[] splitDate = formattedDate.split(",");
-
-
         Log.d("myLog", currentTime.toString());
         Log.d("myLog", formattedDate);
         day.setText(splitDate[0]);
@@ -47,6 +51,11 @@ public class MainActivity2 extends AppCompatActivity {
 
     public void openActivity1() {
         Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void openActivity3() {
+        Intent intent = new Intent(this, MainActivity3.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/gtf/MainActivity3.java
+++ app/src/main/java/com/example/gtf/MainActivity3.java
@@ -3,12 +3,38 @@ package com.example.gtf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity3 extends AppCompatActivity {
+
+    private Button buttonPlay,buttonPause;
+    private Chronometer chronometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        Chronometer simpleChronometer = (Chronometer) findViewById(R.id.chrono); // initiate a chronometer
+        buttonPlay = (Button) findViewById(R.id.btnplay);
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                simpleChronometer.start(); // start a chronometer
+            }
+        });
+
+        buttonPause = (Button) findViewById(R.id.btnpause);
+        buttonPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                simpleChronometer.stop(); // start a chronometer
+            }
+        });
     }
+
 }
